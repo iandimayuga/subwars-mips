991038baf5ead4b3dcded2ed6bb852532da807d9
icd3 <git@iandimayuga.com>
2013-03-16 10:21:55 +0000

    Add math utilities header

---
+++ c/math.h
@@ -0,0 +1,23 @@
+/* math.h
+ * Vector Math and other mathematical utilities
+ */
+
+#ifndef MATH_H
+#define MATH_H
+
+// two-dimensional data structure
+typedef struct {
+    int x;
+    int y;
+} vector;
+
+// vector addition
+vector add(vector v0, vector v1);
+
+// vector-scalar multiplication
+vector mult(vector v, scalar s);
+
+// dot product
+int dot(vector v0, vector v1);
+
+#endif
