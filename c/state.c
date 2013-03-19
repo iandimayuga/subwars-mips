c7a6d2c85f4bc6e98aa0fb4671059392894ddf34
Owen Bell <owenalexbell@gmail.com>
2013-03-18 23:11:54 -0400

    Early version submarine state, not all implemented

--- c/state.c
+++ c/state.c
@@ -4,3 +4,40 @@
 
 #include "state.h"
 
+//this assumes that subs are created in main and passed here to get initialized
+void create_subs(submarine A, submarine B) {
+	A = {/*inline vector declaration*/,/*vector*/, false, false, true, &B};
+	B = {/*vector*/, /*vector*/, false, false, true, &A};
+}
+
+void reset_sub(submarine sub) {
+	B.moving = false;
+	B.fire = false;
+	B.ping = false;
+}
+
+//this method assumes that you issue a move forward command every turn
+void sub_move(submarine sub) {
+	sub.moving = true;
+	/* TODO: vector math here */
+}
+
+void sub_fire(submarine sub){
+	sub.fire = true;
+	struct submarine *other = sub.other_sub;
+	if collision(sub.position, sub.rotation, (other)->position) {
+		(other)->alive = false;
+	}
+}
+
+void sub_rotate_left(submarine sub) {
+	left(sub.rotation);
+}
+
+void sub_rotate_right(submarine sub) {
+	right(sub.rotation);
+}
+
+void sub_ping(submarine sub) {
+	sub.ping = true;
+}
--- c/state.h
+++ c/state.h
@@ -7,10 +7,35 @@
 
 #include "math.h"
 
-// Submarine state structure (16 bytes)
+// Submarine state structure (~19 bytes)
 typedef struct submarine {
     vector position; // 8 bytes
     vector rotation; // 8 bytes
+	bool moving; // 1 byte, this assumes we only have forward and stop
+	bool fire; // 1 byte, did the sub fire last turn
+	bool alive; // 1 byte
+	bool ping; // 1 byte
+	submarine &other_sub; // not sure what the size is
+	//as stands I don't think that we need to store data about the other sub in the struct
 } submarine;
+
+//resets the booleans moving, fire and ping after each round
+void reset_sub(submarine sub);
+
+//initialize two submarine structs at the start of the game
+void create_subs(submarine A, submarine B);
+
+//move the passed submarine
+void sub_move(submarine sub);
+
+//rotate the passed submarine
+void sub_rotate_left(submarine sub);
+
+void sub_rotate_right(submarine sub);
+
+//set the passed submarine to fire
+void sub_fire(submarine sub);
+
+void sub_ping(submarine sub);
 
 #endif // STATE_H
