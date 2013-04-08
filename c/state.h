44c35fbb61ce92b380ed583a6d0e18779c79f0ef
icd3 <git@iandimayuga.com>
2013-04-07 22:35:28 -0400

    Add collision, bounds, and turn flags to submarine

--- c/state.h
+++ c/state.h
@@ -12,14 +12,18 @@
 #define MAP_LEFT 0
 #define MAP_RIGHT 9
 
-// Submarine state structure (~19 bytes)
+// Submarine state structure (21 bytes, or 44 if bools take up a full word)
 typedef struct submarine {
+    int player;
     vector position; // 8 bytes
     vector rotation; // 8 bytes
-    bool move; // 1 byte, this assumes we only have forward and stop
-    bool fire; // 1 byte, did the sub fire last turn
-    bool alive; // 1 byte
+    bool move; // 1 byte, did the sub move
+    bool turn; // 1 byte, did the sub turn
     bool ping; // 1 byte
+    bool fire; // 1 byte, did the sub fire last turn
+    bool bounds; // 1 byte, did the sub attempt to leave the bounds of the map
+    bool collide; // 1 byte, have the subs collided
+    bool alive; // 1 byte
 } submarine;
 
 //resets the booleans moving, fire and ping after each round
@@ -28,17 +32,20 @@ void reset_sub(submarine* sub);
 //initialize two submarine structs at the start of the game
 void create_subs(submarine* A, submarine* B);
 
-//move the passed submarine
+//check if collision has occurred between subs and sink them if necessary
+void check_collision(submarine* A, submarine* B);
+
+//move the submarine
 void sub_move(submarine* sub);
 
-//rotate the passed submarine
+//rotate the submarine
 void sub_rotate_left(submarine* sub);
-
 void sub_rotate_right(submarine* sub);
 
-//set the passed submarine to fire
+//fire the submarine's torpedo and check for hit
 void sub_fire(submarine* sub, submarine* target);
 
+//apply the ping action
 void sub_ping(submarine* sub);
 
 //applies a movement command to a player's sub
