4cec65212141d51908123a50c1b34f5b0dac905e
icd3 <git@iandimayuga.com>
2013-04-21 21:21:27 -0400

    Create state machine MIPS stubs

---
+++ mips/state.asm
@@ -0,0 +1,142 @@
+# state.asm
+# Submarine state and knowledge
+
+# // Submarine state structure (21 bytes, or 44 if bools take up a full word)
+# typedef struct submarine {
+#     int player;
+#     vector position; // 8 bytes
+#     vector rotation; // 8 bytes
+#     bool move; // 1 byte, did the sub move
+#     bool turn; // 1 byte, did the sub turn
+#     bool ping; // 1 byte
+#     bool fire; // 1 byte, did the sub fire last turn
+#     bool bounds; // 1 byte, did the sub attempt to leave the bounds of the map
+#     bool collide; // 1 byte, have the subs collided
+#     bool alive; // 1 byte
+# } submarine;
+
+# //this assumes that subs are created in main and passed here to get initialized
+# void create_subs(submarine* A, submarine* B) {
+#     vector botLeft = {MAP_LEFT, MAP_BOTTOM};
+#     vector topRight = {MAP_RIGHT, MAP_TOP};
+#     vector right = {1, 0};
+#     vector left = {-1, 0};
+#     A->player = 1;
+#     B->player = 2;
+#     A->position = botLeft;
+#     A->rotation = right;
+#     B->position = topRight;
+#     B->rotation = left;
+# 
+#     reset_flags(A);
+#     reset_flags(B);
+# 
+#     A->alive = B->alive = true;
+# }
+
+# void reset_flags(submarine* sub) {
+#     sub->move = false;
+#     sub->turn = false;
+#     sub->ping = false;
+#     sub->fire = false;
+#     sub->bounds = false;
+# }
+
+# void sub_move(submarine* sub, bool dir) {
+#     // Determine direction
+#     vector resultant = {0, 0};
+#     if (dir) {
+# 	resultant = add(sub->position, sub->rotation);
+#     } else {
+#         resultant = subtract(sub->position, sub->rotation);
+#     }
+# 
+#     // Check boundaries
+#     if (resultant.x >= MAP_LEFT &&
+#         resultant.x <= MAP_RIGHT &&
+#         resultant.y >= MAP_BOTTOM &&
+#         resultant.y <= MAP_TOP)
+#     {
+#         // Move if sub would stay in-bounds
+#         sub->move = true;
+#         sub->position = resultant;
+#     } else {
+#         // Notify otherwise, but do not move
+#         sub->bounds = true;
+#     }
+# }
+
+# void check_collision(submarine* A, submarine* B)
+# {
+#     if (equals(A->position, B->position)) {
+#         // A and B occupy the same point in space
+#         A->collide = B->collide = true;
+#     } else if (A->move && B->move) {
+#         // check to see if they may have passed through each other (if they have both moved and are facing away from each other)
+# 
+#         // subtract B from A
+#         vector displacement = subtract(A->position, B->position);
+# 
+#         if (equals(A->rotation, displacement) && equals(B->rotation, mult(displacement, -1)))
+#         {
+#             // A and B passed through each other this turn
+#             A->collide = B->collide = true;
+#         }
+#     }
+# 
+#     // Any collision results in death of both parties
+#     if (A->collide || B->collide) A->alive = B->alive = false;
+# }
+
+# void sub_fire(submarine* sub, submarine* target){
+#     sub->fire = true;
+#     if (collide(sub->position, sub->rotation, target->position)) {
+#         target->alive = false;
+#     }
+# }
+
+# void sub_rotate_left(submarine* sub) {
+#     sub->turn = true;
+#     sub->rotation = left(sub->rotation);
+# }
+
+# void sub_rotate_right(submarine* sub) {
+#     sub->turn = true;
+#     sub->rotation = right(sub->rotation);
+# }
+
+# void sub_ping(submarine* sub) {
+#     sub->ping = true;
+# }
+
+# void evaluate_motion(submarine* sub, int command)
+# {
+#     switch (command)
+#     {
+#         case 0:
+#             sub_move(sub, true);
+#             break;
+#         case 1:
+#             sub_move(sub, false);
+#             break;
+#         case 2:
+#             sub_rotate_left(sub);
+#             break;
+#         case 3:
+#             sub_rotate_right(sub);
+#             break;
+#     }
+# }
+
+# void evaluate_action(submarine* sub, submarine* enemy, int command)
+# {
+#     switch (command)
+#     {
+#         case 4:
+#             sub_ping(sub);
+#             break;
+#         case 5:
+#             sub_fire(sub, enemy);
+#             break;
+#     }
+# }
